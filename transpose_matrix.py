3e2e8d8c67b99b02b0245539474ab54f9922b57c
Karina Pichardo <kpichardo13@berkeley.edu>
2020-10-09 14:01:49 -0700

    added files

---
+++ transpose_matrix.py
@@ -0,0 +1,12 @@
+"""
+Input: [[1,2,3],[4,5,6],[7,8,9]]
+Output: [[1,4,7],[2,5,8],[3,6,9]] 
+"""
+
+def transpose(self, A):
+    """
+    :type A: List[List[int]]
+    :rtype: List[List[int]]
+    """
+    
+
