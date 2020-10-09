4ee638220d4121c1883adda1cc2b31ab2d8bf3b3
Karina Pichardo <kpichardo13@berkeley.edu>
2020-10-09 14:15:03 -0700

    updated for edge cases

--- sort_array_by_parity.py
+++ sort_array_by_parity.py
@@ -1,8 +1,8 @@
-def sortArrayByParity(self, A):
-        """
-        :type A: List[int]
-        :rtype: List[int]
-        """
+class Solution(object):
+   #sort array, even numbers in the front and odd in the end
+   #sort even and odd in ascending order
+   def sortArrayByParity(self, A):
+        #create even and odd array and store respective numbers into array
         even = []
         odd = []
         for num in A:
@@ -10,4 +10,10 @@ def sortArrayByParity(self, A):
                 even.append(num)
             else:
                 odd.append(num)
-        return even + odd
+        #sort elements in array
+        even.sort()
+        odd.sort()
+        return even + odd #combine lists 
+
+sol = Solution()
+print(sol.sortArrayByParity([10,3,2,6,9,22,14,13]))
--- transpose_matrix.py
+++ transpose_matrix.py
@@ -1,12 +1,20 @@
 """
+Example 1:
 Input: [[1,2,3],[4,5,6],[7,8,9]]
 Output: [[1,4,7],[2,5,8],[3,6,9]] 
+
+Example 2:
+Input: [[1,2,3],[4,5,6]]
+Output: [[1,4],[2,5],[3,6]]
 """
 
-def transpose(self, A):
+def transpose(A):
     """
     :type A: List[List[int]]
     :rtype: List[List[int]]
     """
-    
+    return [[j[i] for j in A] for i in range(len(A[0]))]
+A = [[1,2,3],[4,5,6]]
+print(transpose(self, A))
+
 
